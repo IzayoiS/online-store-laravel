63b2e5dad41df7f8ee585120213f002bdfd636f9
IzayoiS <iqbalmhasby54@gmail.com>
2025-08-05 23:34:19 +0700

    feat: redesign login page layout and form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,52 @@
-<x-guest-layout>
+@extends('layouts.auth')
+
+@section('content')
+
+<div class="page-content page-auth">
+      <div class="section-store-auth" data-aos="fade-up">
+        <div class="container">
+          <div class="row align-items-center row-login">
+            <div class="col-lg-6 text-center">
+              <img
+                src="/images/login-placeholder.svg"
+                class="w-50 mb-4 mb-lg-none"
+                alt=""
+              />
+            </div>
+            <div class="col-lg-5">
+              <h2>
+                Belanja kebutuhan utama,<br />
+                menjadi lebih mudah
+              </h2>
+              <form action="" class="mt-3">
+                <div class="form-group">
+                  <label>Email Address</label>
+                  <input type="email" class="form-control w-75" />
+                </div>
+                <div class="form-group">
+                  <label>Password</label>
+                  <input type="password" class="form-control w-75" />
+                </div>
+                <a
+                  href="/dashboard.html"
+                  class="btn btn-success btn-block w-75 mt-4"
+                >
+                  Sign In to My Account
+                </a>
+                <a
+                  href="/register.html"
+                  class="btn btn-signup btn-block w-75 mt-4"
+                >
+                  Sign Up
+                </a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +92,6 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@endsection
