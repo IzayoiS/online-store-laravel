bcf01d031d3b040b51de6c918413afdb8f482229
IzayoiS <iqbalmhasby54@gmail.com>
2025-08-06 22:41:44 +0700

    feat: add DashboardProductController and view

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardProductController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
@@ -19,6 +20,7 @@ Route::get('/success', [CartController::class, 'success'])->name('success');
 Route::get('/register/success', [RegisteredUserController::class, 'success'])->name('register-success');
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product');
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
