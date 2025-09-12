629017609fb282a5d47c45d7fd375db87295270b
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-12 16:24:55 +0700

    feat: add LocationController with provinces and regencies endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\LocationController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('register/check', [RegisteredUserController::class, 'check'])->name('api-register-check');
+Route::get('provinces', [LocationController::class, 'provinces'])->name('api-provinces');
+Route::get('regencies/{provinces_id}', [LocationController::class, 'regencies'])->name('api-regencies');
