85cb2359ac644c5a801c7394be4b36f4f85030a9
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-22 20:21:35 +0700

    refactor: update dashboard product form with validation and price formatting

--- resources/views/pages/dashboard-products-details.blade.php
+++ resources/views/pages/dashboard-products-details.blade.php
@@ -5,144 +5,132 @@
 @endsection
 
 @section('content')
-<div
-class="section-content section-dashboard-home"
-data-aos="fade-up"
->
-<div class="container-fluid">
-    <div class="dashboard-heading">
-    <h2 class="dashboard-title">Shirup Marzan</h2>
-    <p class="dashboard-subtitle">Product Details</p>
-    </div>
-    <div class="dashboard-content">
-    <div class="row">
-        <div class="col-12">
-        <form action="">
-            <div class="card">
-            <div class="card-body">
+    <div class="section-content section-dashboard-home" data-aos="fade-up">
+        <div class="container-fluid">
+            <div class="dashboard-heading">
+                <h2 class="dashboard-title">Shirup Marzan</h2>
+                <p class="dashboard-subtitle">Product Details</p>
+            </div>
+            <div class="dashboard-content">
                 <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                    <label>Product Name</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        value="Papel La Casa"
-                    />
+                    <div class="col-12">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li style="list-style-type: none">{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <form action="{{ route('dashboard-product-update', $product->id) }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <input type="hidden" name="users_id" value="{{ Auth::user()->id }}" />
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Product Name</label>
+                                                <input type="text" class="form-control" value="Papel La Casa"
+                                                    name="name" value="{{ $product->name }}" />
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group">
+                                                <label>Price</label>
+                                                <input type="text" class="form-control" id="price" value="100.00"
+                                                    name="price" {{ $product->price }} />
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Category</label>
+                                                <select name="categories_id" class="form-control">
+                                                    <option value="{{ $product->categories_id }}">Not change
+                                                        ({{ $product->category->name }})</option>
+                                                    @foreach ($categories as $category)
+                                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>Description</label>
+                                                <textarea class="form-control" style="resize: none; height: 130px" name="description">{!! $product->description !!}</textarea>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col text-right">
+                                            <button type="submit" class="btn btn-success px-5 btn-block">
+                                                Update Product
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                    <label>Price</label>
-                    <input
-                        type="number"
-                        class="form-control"
-                        value="100.00"
-                    />
+                <div class="row mt-2">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="gallery-container">
+                                            <img src="/images/product-card-1.svg" alt="" class="w-100" />
+                                            <a href="#" class="delete-gallery">
+                                                <img src="/images/icon-delete.svg" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="gallery-container">
+                                            <img src="/images/product-card-2.svg" alt="" class="w-100" />
+                                            <a href="#" class="delete-gallery">
+                                                <img src="/images/icon-delete.svg" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="gallery-container">
+                                            <img src="/images/product-card-3.svg" alt="" class="w-100" />
+                                            <a href="#" class="delete-gallery">
+                                                <img src="/images/icon-delete.svg" alt="" />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <input type="file" id="file" style="display: none" multiple />
+                                        <button class="btn btn-secondary btn-block mt-3" onclick="thisFileUpload()">
+                                            Add Photo
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                    <label>Kategori</label>
-                    <select name="category" class="form-control">
-                        <option disabled>Select Category</option>
-                    </select>
-                    </div>
-                </div>
-                <div class="col-md-12">
-                    <div class="form-group">
-                    <label>Description</label>
-                    <textarea
-                        class="form-control"
-                        style="resize: none; height: 130px"
-                    ></textarea>
-                    </div>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col text-right">
-                    <button
-                    type="submit"
-                    class="btn btn-success px-5 btn-block"
-                    >
-                    Update Product
-                    </button>
-                </div>
-                </div>
-            </div>
-            </div>
-        </form>
-        </div>
-    </div>
-    <div class="row mt-2">
-        <div class="col-12">
-        <div class="card">
-            <div class="card-body">
-            <div class="row">
-                <div class="col-md-4">
-                <div class="gallery-container">
-                    <img
-                    src="/images/product-card-1.svg"
-                    alt=""
-                    class="w-100"
-                    />
-                    <a href="#" class="delete-gallery">
-                    <img src="/images/icon-delete.svg" alt="" />
-                    </a>
-                </div>
-                </div>
-                <div class="col-md-4">
-                <div class="gallery-container">
-                    <img
-                    src="/images/product-card-2.svg"
-                    alt=""
-                    class="w-100"
-                    />
-                    <a href="#" class="delete-gallery">
-                    <img src="/images/icon-delete.svg" alt="" />
-                    </a>
-                </div>
-                </div>
-                <div class="col-md-4">
-                <div class="gallery-container">
-                    <img
-                    src="/images/product-card-3.svg"
-                    alt=""
-                    class="w-100"
-                    />
-                    <a href="#" class="delete-gallery">
-                    <img src="/images/icon-delete.svg" alt="" />
-                    </a>
-                </div>
-                </div>
-                <div class="col-12">
-                <input
-                    type="file"
-                    id="file"
-                    style="display: none"
-                    multiple
-                />
-                <button
-                    class="btn btn-secondary btn-block mt-3"
-                    onclick="thisFileUpload()"
-                >
-                    Add Photo
-                </button>
-                </div>
-            </div>
             </div>
         </div>
-        </div>
     </div>
-    </div>
-</div>
-</div>
 @endsection
 
 @push('addon-script')
     <script>
-      function thisFileUpload() {
-        document.getElementById("file").click();
-      }
+        function thisFileUpload() {
+            document.getElementById("file").click();
+        }
+        const priceInput = document.getElementById('price');
+
+        priceInput.addEventListener('input', function() {
+            let value = this.value.replace(/\D/g, '');
+
+            this.value = new Intl.NumberFormat().format(value);
+        });
     </script>
 @endpush
