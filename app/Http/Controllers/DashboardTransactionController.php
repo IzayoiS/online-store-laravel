5da8aa1c87ff97b88e10487fbb9a6a4d950f63a1
IzayoiS <iqbalmhasby54@gmail.com>
2025-09-22 22:15:47 +0700

    feat: add transaction management with user-specific data

--- app/Http/Controllers/DashboardTransactionController.php
+++ app/Http/Controllers/DashboardTransactionController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardTransactionController extends Controller
 {
-    public function index() 
+    public function index()
     {
-        return view('pages.dashboard-transactions');
+        $sellTransaction = TransactionDetail::with(['transaction.user', 'product.galleries'])->whereHas('product', function ($product) {
+            $product->where('users_id', Auth::user()->id);
+        })->get();
+        $buyTransaction = TransactionDetail::with(['transaction.user', 'product.galleries'])->whereHas('transaction', function ($transaction) {
+            $transaction->where('users_id', Auth::user()->id);
+        })->get();
+        return view('pages.dashboard-transactions', [
+            'sellTransactions' => $sellTransaction,
+            'buyTransactions' => $buyTransaction,
+        ]);
     }
-    public function details() 
+    public function details()
     {
         return view('pages.dashboard-transactions-details');
     }
